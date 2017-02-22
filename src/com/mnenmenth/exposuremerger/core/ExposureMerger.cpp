2c56a519bc6e98613c0e19520b14ae53862320bd
Mnenmenth <iamrazim12@gmail.com>
2017-02-21 19:53:53 -0500

    Init

---
+++ src/com/mnenmenth/exposuremerger/core/ExposureMerger.cpp
@@ -0,0 +1,13 @@
+/**
+  * Created by Mnenmenth Alkaborin
+  * Please refer to LICENSE file if included
+  * for licensing information
+  * https://github.com/Mnenmenth
+  */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
